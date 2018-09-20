50718b2c56ebd0c82ec7c82844374e87bebf11f2
hongfu12321 <hongfu1220@gmail.com>
2018-09-19 21:25:04 -0700

    modified ft_putstr ft_putstr_fd

--- src/put/ft_putstr_fd.c
+++ src/put/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: fhong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/03 11:04:02 by fhong             #+#    #+#             */
-/*   Updated: 2018/05/03 21:25:13 by fhong            ###   ########.fr       */
+/*   Updated: 2018/09/19 21:24:02 by fhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,5 @@
 void	ft_putstr_fd(char const *s, int fd)
 {
 	if (s)
-		while (*s)
-			ft_putchar_fd(*s++, fd);
+		write(fd, s, ft_strlen(s));
 }
